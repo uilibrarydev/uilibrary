8737035b8c0c385680160864535adf3ed8f2756a
v4lodya <valodyapapikyan@gmail.com>
2022-12-06 17:21:24 +0400

    feature/container component

--- src/components/Button/types.ts
+++ src/components/Button/types.ts
@@ -2,6 +2,6 @@ export type TButtonPropTypes = {
   buttonText: string
   className?: string
   type: 'primary' | 'Warning'
-  iconProps?: { name: string, size: TIconSize, color: string }
+  iconProps?: { name: string; size: TIconSize; color: string }
   onClick: (event: TEventType) => void
 }
