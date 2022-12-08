406af1ef2dfa24ddbf327901a41b6a1ef0b8a7fb
LilitArz <lilitarzzz@gmail.com>
2022-12-08 14:41:44 +0400

    add email and phone icons

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -2,5 +2,7 @@ import { icon } from './icon'
 import { arrow_up } from './arrow_up'
 import { close } from './close'
 import { fileUpload } from './file_upload'
+import { phone } from './phone'
+import { email } from './email'
 
-export const customIcons: TCustomIcons = { icon, arrow_up, close, fileUpload }
+export const customIcons: TCustomIcons = { icon, arrow_up, close, fileUpload, phone, email }
