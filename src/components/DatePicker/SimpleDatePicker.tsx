f13fdbf5b4c01e460c1623e239ca3c9c44186876
RamellaWeb <ramella.web@gmail.com>
2023-02-15 05:43:16 -0800

    updates

--- src/components/DatePicker/SimpleDatePicker.tsx
+++ src/components/DatePicker/SimpleDatePicker.tsx
@@ -5,7 +5,7 @@ import hy from 'date-fns/locale/hy'
 import Input from '../Input'
 import { ISimpleDatePickerProps } from './types'
 import Label from '../../helperComponents/Label'
-import Icon from '../Icon';
+import Icon from '../Icon'
 
 registerLocale('hy', hy)
 
@@ -47,11 +47,11 @@ const SimpleDatePicker = (props: ISimpleDatePickerProps): JSX.Element => {
         selected={moment.isDate(selectedDate) ? selectedDate : new Date()}
         locale="hy"
         customInput={
-            <Input
-                iconProps={{name : 'calendar'}}
-                rightIcon={true}
-                currentValue={selectedDate ? moment(selectedDate.toString()).format(format) : ''}
-            />
+          <Input
+            iconProps={{ name: 'calendar' }}
+            rightIcon={true}
+            currentValue={selectedDate ? moment(selectedDate.toString()).format(format) : ''}
+          />
         }
         {...rest}
         onChange={onChange}
