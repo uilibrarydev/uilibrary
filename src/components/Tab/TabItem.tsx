6cd96edac3ebb7b6feb3b454c39460320dbab217
LilitArz <lilitarzzz@gmail.com>
2023-02-23 17:58:17 +0400

    add tabs functional.

---
+++ src/components/Tab/TabItem.tsx
@@ -0,0 +1,43 @@
+import React from 'react'
+
+import { TTabItemProps } from './types'
+import '../../assets/styles/components/_tab.scss'
+import Icon from '../Icon'
+import Badge from '../Badge'
+
+export const TabItem = (props: TTabItemProps): JSX.Element | null => {
+  const {
+    size = 'large',
+    iconProps,
+    badgeProps,
+    label,
+    className = '',
+    disabled,
+    isSelected,
+    onClick
+  } = props
+
+  return (
+    <div
+      onClick={onClick}
+      className={`tab tab--${size} 
+                        ${disabled ? 'tab--disabled' : ''} 
+                        ${isSelected ? 'tab--selected' : ''} 
+                        ${className}`}
+    >
+      {iconProps?.name ? (
+        <Icon
+          {...iconProps}
+          className="tab__icon mr-8"
+          size={`${size == 'small' ? 'xsmall' : 'small'}`}
+        />
+      ) : null}
+      <span className="tab__label">{label}</span>
+      {badgeProps?.text ? (
+        <Badge {...badgeProps} className="tab__badge ml-8" size="small" type="secondary" />
+      ) : null}
+    </div>
+  )
+}
+
+export default TabItem
