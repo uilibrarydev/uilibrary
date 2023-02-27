e270fbbe1a0387e16c938c78f47d1fd18ce50734
RamellaWeb <ramella.web@gmail.com>
2023-02-27 03:01:27 -0800

    updates

--- src/components/Breadcrumb/index.tsx
+++ src/components/Breadcrumb/index.tsx
@@ -1,26 +1,24 @@
 import React from 'react'
 
-import {TBreadCrumbProps} from './types'
+import { TBreadCrumbProps } from './types'
 import '../../assets/styles/components/_breadcrumb.scss'
 
 export const BreadcrumbComp = (props: TBreadCrumbProps): JSX.Element | null => {
-  const {
-    selectedValue,
-    breadrumbItem,
-    className = '',
-    onSelect,
-  } = props
+  const { selectedValue, breadrumbItem, className = '', onSelect } = props
 
   return (
     <div className={`breadcrumb ${className}`}>
       {breadrumbItem.map((itemInfo) => {
         return (
-          <span className="breadcrumb__item"
-                key={itemInfo.value}
-          >
-            <span className={`breadcrumb__item__inner ${selectedValue === itemInfo.value ? 'selected' : ''}`}
-                  onClick={() => onSelect(itemInfo.value)}
-                  >{itemInfo.label}</span>
+          <span className="breadcrumb__item" key={itemInfo.value}>
+            <span
+              className={`breadcrumb__item__inner ${
+                selectedValue === itemInfo.value ? 'selected' : ''
+              }`}
+              onClick={() => onSelect(itemInfo.value)}
+            >
+              {itemInfo.label}
+            </span>
           </span>
         )
       })}
