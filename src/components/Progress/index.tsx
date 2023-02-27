c4890c3832b0b242d537c1bf4cf1cdb7dee10f7b
RamellaWeb <ramella.web@gmail.com>
2023-02-27 00:20:48 -0800

    added progress bar component

--- src/components/Progress/index.tsx
+++ src/components/Progress/index.tsx
@@ -1,14 +1,37 @@
 import React from 'react'
-import { TProgressPropTypes } from './types'
-import './index.scss'
+import {TProgressPropTypes} from './types'
+import '../../assets/styles/components/_progress.scss'
 
 const Progress = (props: TProgressPropTypes): JSX.Element => {
-  const { percent } = props
-  return (
-    <div className="custom_progress">
-      <div className="custom_progress_filled" style={{ width: `${percent}%` }} />
-    </div>
-  )
+    const {percent, type, size, noText} = props
+
+    const dimension = size === 'small' ? 29 : 45
+    const r = dimension - 5
+
+    const strokeDasharray = `${2 * r * 3.14}px`
+
+    return (
+        <div className={`progress-bar progress-bar--${type} progress-bar--${size}`}>
+            {type == 'circle' ? (
+                <svg>
+                    <circle cx={dimension} cy={dimension} r={r}></circle>
+                    <circle cx={dimension} cy={dimension} r={r} style={{
+                        strokeDasharray,
+                        strokeDashoffset: `calc(${strokeDasharray} - (${strokeDasharray} * ${percent}) / 100)`
+                    }}></circle>
+                </svg>
+            ) : (
+                <div className="progress-bar__inner">
+                    <div className="progress-bar__filled" style={{width: `${percent}%`}}></div>
+                </div>
+            )}
+            {!noText ? (
+                <span className="progress-bar__percent">{percent}%</span>
+                ) : null
+            }
+        </div>
+
+    )
 }
 
 export default Progress
