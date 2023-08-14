cc8c9457e687660ba4ac2e93ee8b57e23bc4e01c
Karo <kghulyan93@gmail.com>
2023-08-14 18:41:55 +0400

    change version

--- src/components/Link/index.tsx
+++ src/components/Link/index.tsx
@@ -5,7 +5,7 @@ import '../../assets/styles/components/_link.scss'
 const Link: React.FC<LinkPropTypes> = (props: LinkPropTypes) => {
   const { children, className = '', url, dataId = '' } = props
   return (
-    <a href={url} className={`link ${className}`} data-id={dataId}>
+    <a href={url} className={`link ${className}`} data-id={`${dataId}-link`}>
       {children}
     </a>
   )
