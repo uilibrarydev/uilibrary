1065ed14e6f61e94dad8b67e25d42f2696ef8878
lilit.arzumanyan <lilit.arzumanyan@ameriabank.am>
2023-08-18 15:52:30 +0400

    change snackbar logic
     - export `useGetSnackbar` customHook

--- src/components/Snackbar/index.tsx
+++ src/components/Snackbar/index.tsx
@@ -1,70 +1,76 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import { ToastContainer, toast, Slide } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
 
 import { Text, Icon, Button } from '../index'
 
 import '../../assets/styles/components/_snackbar.scss'
-import { TSnackbarProps } from './types'
+import { TSnackbarProps, TToastProps } from './types'
 import { ICONS_MAPPING, TYPE_MAPPING } from './consts'
 
-export const Snackbar = (props: TSnackbarProps): JSX.Element | null => {
-  const {
-    text,
-    actionProps,
-    closeSnackbar,
-    duration = 6000,
-    type = 'information',
-    position = 'bottom-center'
-  } = props
+export const useGetSnackbar = (
+  props: TSnackbarProps
+): {
+  Comp: () => JSX.Element
+  _toast: (toastProps: TToastProps) => void
+} => {
+  const { duration = 6000, position = 'bottom-center' } = props
 
-  const CustomToastWithLink = () => (
-    <div className="snackbar">
-      <Icon name={ICONS_MAPPING[type]} type={TYPE_MAPPING[type]} className="mr-16" size="medium" />
-
-      <Text
-        className="snackbar__text"
-        type="primary"
-        size="standard"
-        weight="regular"
-        lineHeight="medium"
-      >
-        {text}
-      </Text>
-      {actionProps ? (
-        <Button
-          size="small"
-          type="tertiary"
-          {...actionProps}
-          className="ml-16"
-          onClick={(e) => {
-            toast.dismiss()
-            actionProps?.onClick?.(e)
-          }}
+  const CustomToast = ({ actionProps, toastId, type = 'information', text }: TToastProps) => {
+    return (
+      <div className="snackbar">
+        <Icon
+          name={ICONS_MAPPING[type]}
+          type={TYPE_MAPPING[type]}
+          className="mr-16"
+          size="medium"
         />
-      ) : null}
-    </div>
-  )
 
-  useEffect(() => {
-    toast(CustomToastWithLink, {
-      onClose: closeSnackbar,
+        <Text
+          className="snackbar__text"
+          type="primary"
+          size="standard"
+          weight="regular"
+          lineHeight="medium"
+        >
+          {text}
+        </Text>
+        {actionProps ? (
+          <Button
+            size="small"
+            type="tertiary"
+            {...actionProps}
+            className="ml-16"
+            onClick={(e) => {
+              toast.dismiss(toastId)
+              actionProps?.onClick?.(e)
+            }}
+          />
+        ) : null}
+      </div>
+    )
+  }
+
+  const _toast = (toastProps: TToastProps) => {
+    const { toastId, closeSnackbar } = toastProps
+    toast(() => CustomToast(toastProps), {
+      onClose: () => closeSnackbar?.(toastId),
       bodyClassName: '__body',
-      className: '_container'
+      className: '_container',
+      toastId
     })
-  }, [])
+  }
 
-  return (
+  const Comp = () => (
     <ToastContainer
+      theme="light"
+      hideProgressBar
+      transition={Slide}
       position={position}
       autoClose={duration}
-      hideProgressBar
       closeButton={() => null}
-      transition={Slide}
-      theme="light"
-      className="snackbar"
     />
   )
-}
 
-export default Snackbar
+  return { Comp, _toast }
+}
