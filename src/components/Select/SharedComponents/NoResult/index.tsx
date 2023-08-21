2f3886fda84166e16b5f2d023a1b2c96c1318570
Ramella.Naltakyan <Ramella.Naltakyan@ameriabank.am>
2023-08-21 18:14:25 +0400

    updates for icons, noResult, Counter components

--- src/components/Select/SharedComponents/NoResult/index.tsx
+++ src/components/Select/SharedComponents/NoResult/index.tsx
@@ -1,29 +1,31 @@
 import React from 'react'
 import Text from '../../../Text'
 import Image from '../../../Image'
-import Button from '../../../Button';
-import './index.scss';
+import Button from '../../../Button'
+import './index.scss'
 
 type TNoResultProps = {
-    type?: string
-    mainMessage: string
-    paragraphMessage?: string
-    buttonText?: string
+  type?: string
+  mainMessage: string
+  paragraphMessage?: string
+  buttonText?: string
 }
 
 export const NoResult = (props: TNoResultProps): JSX.Element => {
-    const {type = 'large', mainMessage, paragraphMessage, buttonText} = props
+  const { type = 'large', mainMessage, paragraphMessage, buttonText } = props
 
-    return (
-        <div className={`no-result ${type == 'large' ? 'no-result--large' : 'no-result--small'}`}>
-            <div className="no-result__icon mb-8">
-                <Image imagePath="static/media/src/assets/images/no-result.svg"/>
-            </div>
-            <Text size={type == 'large' ? 'large' : 'small'} weight="bold">
-                {mainMessage}
-            </Text>
-            {paragraphMessage ? <Text size={type == 'large' ? 'small' : 'xsmall'}>{paragraphMessage}</Text> : null}
-            {buttonText ? <Button buttonText={buttonText} size="medium" className="mt-4"/> : null}
-        </div>
-    )
+  return (
+    <div className={`no-result ${type == 'large' ? 'no-result--large' : 'no-result--small'}`}>
+      <div className="no-result__icon mb-8">
+        <Image imagePath="static/media/src/assets/images/no-result.svg" />
+      </div>
+      <Text size={type == 'large' ? 'large' : 'small'} weight="bold">
+        {mainMessage}
+      </Text>
+      {paragraphMessage ? (
+        <Text size={type == 'large' ? 'small' : 'xsmall'}>{paragraphMessage}</Text>
+      ) : null}
+      {buttonText ? <Button buttonText={buttonText} size="medium" className="mt-4" /> : null}
+    </div>
+  )
 }
