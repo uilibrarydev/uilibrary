4dbec830db2892b0cf96bc0b181759540817ddc7
Karo <kghulyan93@gmail.com>
2023-08-23 14:36:23 +0400

    temporary hide img and progress

--- src/stories/FileUpload.stories.tsx
+++ src/stories/FileUpload.stories.tsx
@@ -15,7 +15,7 @@ const getFiles = (files: FileList) => {
 const Template = (args): JSX.Element => {
   return (
     <div style={{ width: 400 }}>
-      <FileUploadComp {...args}  getFiles={getFiles} />
+      <FileUploadComp {...args} getFiles={getFiles} />
     </div>
   )
 }
@@ -25,5 +25,5 @@ FileUpload.args = {
   label: 'Label',
   buttonText: 'Attach file',
   isFileUploaded: false,
-  allowedTypes:['PDF']
+  allowedTypes: ['PDF']
 }
