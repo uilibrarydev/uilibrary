532989e241970f4d5be496004e2ee70cdae7a69c
Ramella.Naltakyan <Ramella.Naltakyan@ameriabank.am>
2023-09-07 00:53:17 +0400

    update

--- src/components/Divider/index.tsx
+++ src/components/Divider/index.tsx
@@ -1,23 +1,23 @@
 import React from 'react'
-import {TDividerProps} from './types'
+import { TDividerProps } from './types'
 import '../../assets/styles/components/_divider.scss'
-import classnames from 'classnames';
+import classnames from 'classnames'
 
 const Divider = (props: TDividerProps): JSX.Element => {
-    const {type, isHorizontal, className} = props
-    return (
-        <hr
-            className={classnames(
-                'divider',
-                `divider--${type}`,
-                {
-                    'divider--horizontal': isHorizontal,
-                    'divider--vertical': !isHorizontal
-                },
-                className
-            )}
-        />
-    )
+  const { type, isHorizontal, className } = props
+  return (
+    <hr
+      className={classnames(
+        'divider',
+        `divider--${type}`,
+        {
+          'divider--horizontal': isHorizontal,
+          'divider--vertical': !isHorizontal
+        },
+        className
+      )}
+    />
+  )
 }
 
 export default Divider
