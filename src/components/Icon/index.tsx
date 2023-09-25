c548ddec7b0567a4ee8ce49e52b543ae0cc0cb41
lilit.arzumanyan <lilit.arzumanyan@ameriabank.am>
2023-09-25 14:24:59 +0400

    fix collapse title styles
     - add `id` prop for `Icon` component

--- src/components/Icon/index.tsx
+++ src/components/Icon/index.tsx
@@ -3,10 +3,10 @@ import { IconPropTypes } from './types'
 import './index.scss'
 
 const Icon = (props: IconPropTypes): JSX.Element => {
-  const { name, type, size = 'medium', onClick, className = '', refHandler } = props
+  const { name, type, size = 'medium', onClick, className = '', refHandler, id } = props
 
   return (
-    <span className={`icon icon--${size} ${type ? `color-${type}` : ''} ${className}`}>
+    <span className={`icon icon--${size} ${type ? `color-${type}` : ''} ${className}`} id={`${id}`}>
       <i ref={refHandler} onClick={onClick} className={`icon-${name}`} />
     </span>
   )
