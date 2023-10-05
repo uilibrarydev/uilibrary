282d1ee4baacb2648e843942cbe8d2ca690bdb05
Ramella.Naltakyan <Ramella.Naltakyan@ameriabank.am>
2023-10-05 20:45:28 +0400

    update

--- src/components/Empty/index.tsx
+++ src/components/Empty/index.tsx
@@ -4,13 +4,17 @@ import Image from '../Image'
 import Button from '../Button'
 import { TEmptyProps } from './types'
 import './index.scss'
-import classNames from 'classnames';
+import classNames from 'classnames'
 
 const Empty = (props: TEmptyProps): JSX.Element => {
   const { type = 'large', mainMessage, paragraphMessage, buttonText } = props
 
   return (
-    <div className={classNames(`no-result ${type == 'large' ? 'no-result--large' : 'no-result--small'}, className`)}>
+    <div
+      className={classNames(
+        `no-result ${type == 'large' ? 'no-result--large' : 'no-result--small'}, className`
+      )}
+    >
       <div className="no-result__icon mb-8">
         <Image imagePath="static/media/src/assets/images/no-result.svg" />
       </div>
