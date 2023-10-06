17b958629d91b55abcfac93f72afa5f71e8d5a8a
Ramella.Naltakyan <Ramella.Naltakyan@ameriabank.am>
2023-10-06 14:51:12 +0400

    updates for empty component

--- src/components/Empty/index.tsx
+++ src/components/Empty/index.tsx
@@ -7,12 +7,11 @@ import './index.scss'
 import classNames from 'classnames'
 
 const Empty = (props: TEmptyProps): JSX.Element => {
-  const { type = 'large', mainMessage, paragraphMessage, buttonText, className } = props
+  const { type = 'large', mainMessage, paragraphMessage, buttonProps, className } = props
 
   return (
     <div
-      className={classNames(
-        `no-result ${type == 'large' ? 'no-result--large' : 'no-result--small'}`,
+      className={classNames(`no-result ${type == 'large' ? 'no-result--large' : 'no-result--small'}`,
         className
       )}
     >
@@ -23,9 +22,9 @@ const Empty = (props: TEmptyProps): JSX.Element => {
         {mainMessage}
       </Text>
       {paragraphMessage ? (
-        <Text size={type == 'large' ? 'small' : 'xsmall'}>{paragraphMessage}</Text>
+        <Text size={type == 'large' ? 'small' : 'xsmall'} className={type == 'large' ? 'mt-8' : 'mt-4'}>{paragraphMessage}</Text>
       ) : null}
-      {buttonText ? <Button buttonText={buttonText} size="medium" className="mt-4" /> : null}
+      {buttonProps ? <Button {...buttonProps} size="medium" className={type == 'large' ? 'mt-16' : 'mt-8'} /> : null}
     </div>
   )
 }
