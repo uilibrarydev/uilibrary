045b52e75541973d718690de1ee9c5a8786095c9
lilit.arzumanyan <lilit.arzumanyan@ameriabank.am>
2023-10-17 14:30:06 +0400

    add stepper initial functional

---
+++ src/components/ProgressStep/index.tsx
@@ -0,0 +1,33 @@
+import React, { ReactElement } from 'react'
+
+import { Step } from './Step'
+
+import { TProgressStepProps } from './types'
+import '../../assets/styles/components/_progresStepper.scss'
+
+export const ProgressStep = (props: TProgressStepProps): ReactElement | null => {
+  const { steps, stepType, activeStep, setActiveStep, completedValues } = props
+
+  return (
+    <div className="progress_steper">
+      {steps.map((step, index) => {
+        const isFirstStep = index === 0
+        const isLastStep = index === steps.length - 1
+
+        return (
+          <Step
+            step={step}
+            index={index}
+            key={step.value}
+            activeStep={activeStep}
+            onStepClick={setActiveStep}
+            stepType={stepType}
+            hasLeftLine={!isFirstStep}
+            hasRightLine={!isLastStep}
+            completedValues={completedValues}
+          />
+        )
+      })}
+    </div>
+  )
+}
