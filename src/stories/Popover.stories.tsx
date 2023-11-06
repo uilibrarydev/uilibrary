0b11e6ad94a2e8a86245dbeb189b491458041cb1
Karo <kghulyan93@gmail.com>
2023-11-06 18:26:12 +0400

    change version

--- src/stories/Popover.stories.tsx
+++ src/stories/Popover.stories.tsx
@@ -46,5 +46,8 @@ export const Popover = Template.bind({})
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
 Popover.args = {
+    linkAddons:{
+        url:'dasdsa',
+    },
   text: 'Lorem ipsum dolor sit amet. Ea accusamus laboriosam sit sintEa accusamus laboriosam sit sint expedita et numquam sunt. A sunt assumenda id quos sequi qui eius nulla. Est saepe dolorem qui nemo enim id velit voluptas.Est saepe dolorem qui nemo enim id velit voluptas.Est saepe dolorem qui nemo enim id velit voluptas.Est saepe dolorem.'
 }
