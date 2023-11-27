cae6e5c99a24c9e9fe757cfe182b6c1a30432653
Ramella.Naltakyan <Ramella.Naltakyan@ameriabank.am>
2023-11-27 19:04:26 +0400

    update

--- src/hooks/index.tsx
+++ src/hooks/index.tsx
@@ -8,7 +8,7 @@ import { useOnOutsideClick } from './useOnOutsideClick'
 import { useGetHasBottomSpace } from './useGetHasBottomSpace'
 import { useHideOnScroll } from './useHideOnScroll'
 import { useHideBodyScroll } from './useHideBodyScroll'
-import {useScreenSize} from './useScreenSize'
+import { useScreenSize } from './useScreenSize'
 
 export {
   useFormContext,
--- src/stories/Pagination.stories.tsx
+++ src/stories/Pagination.stories.tsx
@@ -1,29 +1,29 @@
 import React from 'react'
 import { Icon, Pagination as PaginationComp } from '../components'
 export default {
-    title: 'Pagination',
-    component: PaginationComp,
-    argTypes: {
-        size: {
-            options: ['large', 'medium', 'small'],
-            control: { type: 'radio' }
-        }
+  title: 'Pagination',
+  component: PaginationComp,
+  argTypes: {
+    size: {
+      options: ['large', 'medium', 'small'],
+      control: { type: 'radio' }
     }
+  }
 }
 const Template = (args) => (
-    <PaginationComp
-        {...args}
-        handleChangePage={(item) => {
-            console.log(item)
-        }}
-    />
+  <PaginationComp
+    {...args}
+    handleChangePage={(item) => {
+      console.log(item)
+    }}
+  />
 )
 export const Pagination = Template.bind({})
 
 Pagination.args = {
-    size: 'large',
-    totalCount: 100,
-    nextLabel: <Icon name="arrow-right" />,
-    prevLabel: <Icon name="arrow-left" />,
-    breakLabel: <Icon name="more" />
+  size: 'large',
+  totalCount: 100,
+  nextLabel: <Icon name="arrow-right" />,
+  prevLabel: <Icon name="arrow-left" />,
+  breakLabel: <Icon name="more" />
 }
