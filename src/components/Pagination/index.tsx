31e73594573d0c9d18162b14b1f591a95a56c2e5
valodyap <valodya.papikyan@ameriabank.am>
2023-11-27 19:22:48 +0400

    feature/pagination component

--- src/components/Pagination/index.tsx
+++ src/components/Pagination/index.tsx
@@ -2,9 +2,9 @@ import React from 'react'
 import ReactPaginate from 'react-paginate'
 import Icon from '../Icon'
 import {
-  NEXT_LABEL_DEFAULT_TEXT,
-  PREVIOUS_LABEL_DEFAULT_TEXT,
-  PAGE_RANGE_DISPLAYED
+    NEXT_LABEL_DEFAULT_TEXT,
+    PREVIOUS_LABEL_DEFAULT_TEXT,
+    PAGE_RANGE_DISPLAYED
 } from './consts'
 import { TPaginationProps } from './types'
 import '../../assets/styles/components/_pagination.scss'
@@ -13,49 +13,51 @@ import Text from '../Text'
 import SingleSelect from '../Select/SingleSelect'
 
 export const Pagination = (props: TPaginationProps): JSX.Element | null => {
-  const {
-    nextLabel = NEXT_LABEL_DEFAULT_TEXT,
-    prevLabel = PREVIOUS_LABEL_DEFAULT_TEXT,
-    totalCount,
-    page = 2,
-    handleChangePage,
-    breakLabel = <Icon name="more" />,
-    containerClassName = '',
-    activeClassName = '',
-    size = 'large'
-  } = props
-  const handlePageChange = (selected: { selected: number }) => {
-    handleChangePage(selected.selected + 1)
-  }
-  return (
-    <div className="pagination-wrapper flexbox justify-content--between">
-      <div className="pagination-wrapper__info flexbox align-items--center mr-24">
-        <div className="flexbox align-items--center mr-24">
-          <Text type={'secondary'} className={'mr-8'}>
-            Rows per page
-          </Text>
-          {/*<SingleSelect options={} />*/}
+    const {
+        nextLabel = NEXT_LABEL_DEFAULT_TEXT,
+        prevLabel = PREVIOUS_LABEL_DEFAULT_TEXT,
+        totalCount,
+        page = 2,
+        handleChangePage,
+        breakLabel = <Icon name="more" />,
+        containerClassName = '',
+        activeClassName = '',
+        size = 'large'
+    } = props
+    const handlePageChange = (selected: { selected: number }) => {
+        handleChangePage(selected.selected + 1)
+    }
+    return (
+        <div className="pagination-wrapper flexbox git status">
+            <div className="pagination-wrapper__info flexbox align-items--center mr-24">
+                <div className="flexbox align-items--center mr-24">
+                    <Text type={'secondary'} className={'mr-8'}>
+                        <></>
+                        {/* Rows per page */}
+                    </Text>
+                    {/* <SingleSelect options={[]} /> */}
+                </div>
+                <Text type={'secondary'}>
+                    <>
+                        {/* {page === 1 ? '' : page - 1}1 - {+`${page}0` > totalCount ? totalCount : `${page}0`} of{' '} */}
+                        {/* {totalCount} */}
+                    </>
+                </Text>
+            </div>
+            <ReactPaginate
+                pageCount={totalCount}
+                pageRangeDisplayed={PAGE_RANGE_DISPLAYED}
+                marginPagesDisplayed={1}
+                onPageChange={handlePageChange}
+                containerClassName={classNames(`pagination pagination--${size}`, containerClassName)}
+                activeClassName={activeClassName || 'active'}
+                previousLabel={prevLabel}
+                nextLabel={nextLabel}
+                breakClassName="page-item"
+                breakLinkClassName="page-link"
+                breakLabel={breakLabel}
+                initialPage={0}
+            />
         </div>
-        <Text type={'secondary'}>
-          <>
-            {page === 1 ? '' : page - 1}1 - {+`${page}0` > totalCount ? totalCount : `${page}0`} of{' '}
-            {totalCount}
-          </>
-        </Text>
-      </div>
-      <ReactPaginate
-        pageCount={totalCount}
-        pageRangeDisplayed={PAGE_RANGE_DISPLAYED}
-        onPageChange={handlePageChange}
-        containerClassName={classNames(`pagination pagination--${size}`, containerClassName)}
-        activeClassName={activeClassName || 'active'}
-        previousLabel={prevLabel}
-        nextLabel={nextLabel}
-        breakClassName="page-item"
-        breakLinkClassName="page-link"
-        breakLabel={breakLabel}
-        initialPage={0}
-      />
-    </div>
-  )
+    )
 }
