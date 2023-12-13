a7f065182a3fdb19e709cf9e01fcd797227a4160
rubengabrielyan <gabrielianruben@gmail.com>
2023-12-13 11:39:06 +0400

    use children prop instead of Child prop

--- src/components/SideNavigation/NavigationItem/types.ts
+++ src/components/SideNavigation/NavigationItem/types.ts
@@ -1,29 +1,29 @@
 export enum NavigationItemTypes {
-  MAIN = 'main',
-  SUB = 'sub',
-  BLOCK_HEADER = 'block-header',
-  USER = 'user',
-  PRODUCT = 'product',
-  ACTION = 'action'
+    MAIN = 'main',
+    SUB = 'sub',
+    BLOCK_HEADER = 'block-header',
+    USER = 'user',
+    PRODUCT = 'product',
+    ACTION = 'action'
 }
 
 export interface TNavigationLinkPropTypes {
-  As: () => JSX.Element
-  type:
-    | NavigationItemTypes.MAIN
-    | NavigationItemTypes.SUB
-    | NavigationItemTypes.BLOCK_HEADER
-    | NavigationItemTypes.USER
-    | NavigationItemTypes.PRODUCT
-    | NavigationItemTypes.ACTION
-  open: boolean
-  iconName?: string
-  showBadge?: boolean
-  expandable?: boolean
-  showAction?: boolean
-  actionElm?: JSX.Element
-  userImage?: JSX.Element
-  active?: boolean
-  badgeContent?: string
-  Child?: () => JSX.Element
+    As: () => JSX.Element
+    type:
+        | NavigationItemTypes.MAIN
+        | NavigationItemTypes.SUB
+        | NavigationItemTypes.BLOCK_HEADER
+        | NavigationItemTypes.USER
+        | NavigationItemTypes.PRODUCT
+        | NavigationItemTypes.ACTION
+    open: boolean
+    iconName?: string
+    showBadge?: boolean
+    expandable?: boolean
+    showAction?: boolean
+    actionElm?: JSX.Element
+    userImage?: JSX.Element
+    active?: boolean
+    badgeContent?: string
+    children?: JSX.Element
 }
