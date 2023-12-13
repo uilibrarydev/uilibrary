a928fd2b0f1d96ae3259fba18bd0f085e43a77c0
rubengabrielyan <gabrielianruben@gmail.com>
2023-12-13 09:38:26 +0400

    use classNames lib

--- src/components/SideNavigation/NavigationItem/index.tsx
+++ src/components/SideNavigation/NavigationItem/index.tsx
@@ -1,74 +1,74 @@
-import React, { useState } from 'react'
+import React, {useState} from 'react'
 import classNames from 'classnames'
-import { NavigationItemTypes, TNavigationLinkPropTypes } from './types'
+import {NavigationItemTypes, TNavigationLinkPropTypes} from './types'
 import './index.scss'
 import Icon from '../../Icon'
 import Badge from '../../Badge'
 
 const NavigationItem = (props: TNavigationLinkPropTypes) => {
-  const {
-    As,
-    type,
-    open,
-    iconName,
-    showBadge,
-    expandable = false,
-    showAction = false,
-    actionElm,
-    userImage,
-    active = false,
-    badgeContent,
-    Child
-  } = props
+    const {
+        As,
+        type,
+        open,
+        iconName,
+        showBadge,
+        expandable = false,
+        showAction = false,
+        actionElm,
+        userImage,
+        active = false,
+        badgeContent,
+        Child
+    } = props
 
-  const [childOpen, setChildOpen] = useState(false)
+    const [childOpen, setChildOpen] = useState(false)
 
-  return (
-    <>
-      <div
-        className={classNames('navigation--wrapper', `navigation--wrapper--${type}`)}
-        onClick={() => setChildOpen(!childOpen)}
-      >
-        <div
-          className={classNames(
-            'navigation--item',
-            `navigation--item--${type}`,
-            expandable && 'expandable',
-            active && 'active'
-          )}
-        >
-          <div className="navigation--item--wrapper">
-            {!open && type === NavigationItemTypes.BLOCK_HEADER && As()}
-            {open &&
-              type === NavigationItemTypes.BLOCK_HEADER &&
-              showAction &&
-              actionElm &&
-              actionElm}
-            {iconName && type !== NavigationItemTypes.USER ? (
-              <Icon name={iconName} size="small" />
-            ) : (
-              userImage
-            )}
-            {open && As()}
-            {type === NavigationItemTypes.USER && iconName && open && (
-              <Icon name={iconName} size="small" />
-            )}
-          </div>
-          {showBadge && badgeContent && open && (
-            <Badge type="primary" text={badgeContent} size="small" />
-          )}
-          {expandable && open && (
-            <div className={`navigation--item--expandable--icon ${childOpen ? 'open' : ''}`}>
-              <Icon name="arrow-down" />
+    return (
+        <>
+            <div
+                className={classNames('navigation--wrapper', `navigation--wrapper--${type}`)}
+                onClick={() => setChildOpen(!childOpen)}
+            >
+                <div
+                    className={classNames(
+                        'navigation--item',
+                        `navigation--item--${type}`,
+                        expandable && 'expandable',
+                        active && 'active'
+                    )}
+                >
+                    <div className="navigation--item--wrapper">
+                        {!open && type === NavigationItemTypes.BLOCK_HEADER && As()}
+                        {open &&
+                            type === NavigationItemTypes.BLOCK_HEADER &&
+                            showAction &&
+                            actionElm &&
+                            actionElm}
+                        {iconName && type !== NavigationItemTypes.USER ? (
+                            <Icon name={iconName} size="small"/>
+                        ) : (
+                            userImage
+                        )}
+                        {open && As()}
+                        {type === NavigationItemTypes.USER && iconName && open && (
+                            <Icon name={iconName} size="small"/>
+                        )}
+                    </div>
+                    {showBadge && badgeContent && open && (
+                        <Badge type="primary" text={badgeContent} size="small"/>
+                    )}
+                    {expandable && open && (
+                        <div className={classNames('navigation--item--expandable--icon', childOpen && 'open')}>
+                            <Icon name="arrow-down"/>
+                        </div>
+                    )}
+                </div>
+                {Child && (
+                    <div className={classNames('navigation--items--childs', childOpen && 'active')}>{Child()}</div>
+                )}
             </div>
-          )}
-        </div>
-        {Child && (
-          <div className={`navigation--items--childs ${childOpen ? 'active' : ''}`}>{Child()}</div>
-        )}
-      </div>
-    </>
-  )
+        </>
+    )
 }
 
 export default NavigationItem
--- src/stories/SideNavigation.stories.tsx
+++ src/stories/SideNavigation.stories.tsx
@@ -1,160 +1,166 @@
-import React, {useState} from 'react'
-import {Avatar, Button, NavigationItem, SideNavigation as SideNavigationItemComp, Text} from '../components'
+import React, { useState } from 'react'
+import {
+  Avatar,
+  Button,
+  NavigationItem,
+  SideNavigation as SideNavigationItemComp,
+  Text
+} from '../components'
 import Block from '../components/SideNavigation/Block'
 import logo from '../assets/images/logo.svg'
 import logoClosed from '../assets/images/logo-closed.svg'
 import avatar from '../assets/images/avatar.jpg'
-import {NavigationItemTypes} from '../components/SideNavigation/NavigationItem/types'
+import { NavigationItemTypes } from '../components/SideNavigation/NavigationItem/types'
 
 export default {
-    title: 'Side Navigation',
-    component: SideNavigationItemComp,
-    argTypes: {
-        showAction: {
-            options: [true, false]
-        }
+  title: 'Side Navigation',
+  component: SideNavigationItemComp,
+  argTypes: {
+    showAction: {
+      options: [true, false]
     }
+  }
 }
 
 const Template = (args): JSX.Element => {
-    const [open, setOpen] = useState<boolean>(true)
+  const [open, setOpen] = useState<boolean>(true)
 
-    return (
-        <SideNavigationItemComp setOpen={setOpen} open={open} {...args}>
-            <main>
-                <NavigationItem
-                    As={() => (
-                        <a href="/https://ameriabank.am">
-                            <img src={open ? logo : logoClosed} alt="Product Logo"/>
-                        </a>
-                    )}
-                    type={NavigationItemTypes.BLOCK_HEADER}
+  return (
+    <SideNavigationItemComp setOpen={setOpen} open={open} {...args}>
+      <main>
+        <NavigationItem
+          As={() => (
+            <a href="/https://ameriabank.am">
+              <img src={open ? logo : logoClosed} alt="Product Logo" />
+            </a>
+          )}
+          type={NavigationItemTypes.BLOCK_HEADER}
+          open={open}
+          showAction={args.showAction}
+          actionElm={args.actionElm}
+        />
+        <Block open={open} label="Finance">
+          <>
+            <NavigationItem
+              As={() => <a href="/https://ameriabank.am">Accounts</a>}
+              type={NavigationItemTypes.MAIN}
+              iconName="document"
+              open={open}
+              showAction={args.showAction}
+              actionElm={args.actionElm}
+              active
+            />
+            <NavigationItem
+              As={() => <div>Cards</div>}
+              type={NavigationItemTypes.MAIN}
+              iconName="home"
+              open={open}
+              expandable
+              Child={() => (
+                <>
+                  <NavigationItem
+                    As={() => <a href="/https://ameriabank.am">Accounts</a>}
+                    type={NavigationItemTypes.MAIN}
+                    iconName="document"
                     open={open}
-                    showAction={args.showAction}
-                    actionElm={args.actionElm}
-                />
-                <Block open={open} label="Finance">
-                    <>
-                        <NavigationItem
-                            As={() => <a href="/https://ameriabank.am">Accounts</a>}
-                            type={NavigationItemTypes.MAIN}
-                            iconName="document"
-                            open={open}
-                            showAction={args.showAction}
-                            actionElm={args.actionElm}
-                            active
-                        />
-                        <NavigationItem
-                            As={() => <div>Cards</div>}
-                            type={NavigationItemTypes.MAIN}
-                            iconName="home"
-                            open={open}
-                            expandable
-                            Child={() => (
-                                <>
-                                    <NavigationItem
-                                        As={() => <a href="/https://ameriabank.am">Accounts</a>}
-                                        type={NavigationItemTypes.MAIN}
-                                        iconName="document"
-                                        open={open}
-                                    />
-                                    <NavigationItem
-                                        As={() => <a href="/https://ameriabank.am">Savings</a>}
-                                        type={NavigationItemTypes.MAIN}
-                                        iconName="savings"
-                                        open={open}
-                                    />
-                                </>
-                            )}
-                        />
-                        <NavigationItem
-                            As={() => <a href="/https://ameriabank.am">Savings</a>}
-                            type={NavigationItemTypes.MAIN}
-                            iconName="savings"
-                            open={open}
-                            showBadge
-                            badgeContent={'888+'}
-                        />
-                    </>
-                </Block>
-                <Block open={open} label="Finance">
-                    <>
-                        <NavigationItem
-                            As={() => <a href="/https://ameriabank.am">Overdraft</a>}
-                            type={NavigationItemTypes.MAIN}
-                            iconName="wallet"
-                            open={open}
-                        />
-                        <NavigationItem
-                            As={() => <a href="/https://ameriabank.am">Mortgage</a>}
-                            type={NavigationItemTypes.MAIN}
-                            iconName="home"
-                            open={open}
-                        />
-                        <NavigationItem
-                            As={() => <div>Car Leon</div>}
-                            type={NavigationItemTypes.MAIN}
-                            iconName="savings"
-                            open={open}
-                            expandable
-                            Child={() => (
-                                <>
-                                    <NavigationItem
-                                        As={() => <a href="/https://ameriabank.am">Savings</a>}
-                                        type={NavigationItemTypes.MAIN}
-                                        iconName="savings"
-                                        open={open}
-                                    />
-                                    <NavigationItem
-                                        As={() => <a href="/https://ameriabank.am">Accounts</a>}
-                                        type={NavigationItemTypes.MAIN}
-                                        iconName="document"
-                                        open={open}
-                                    />
-                                </>
-                            )}
-                        />
-                    </>
-                </Block>
-            </main>
-            <footer>
-                <Block>
-                    <>
-                        <NavigationItem
-                            type={NavigationItemTypes.MAIN}
-                            open={open}
-                            iconName={'mail'}
-                            As={() => <a href="/https://ameriabank.am">Messages</a>}
-                        />
-                        <NavigationItem
-                            type={NavigationItemTypes.MAIN}
-                            open={open}
-                            iconName={'notification2'}
-                            As={() => <a href="https://ameriabank.am">Notifications</a>}
-                        />
-                        <NavigationItem
-                            type={NavigationItemTypes.MAIN}
-                            open={open}
-                            iconName={'settings'}
-                            As={() => <a href="https://ameriabank.am">Settings</a>}
-                        />
-                        <NavigationItem
-                            type={NavigationItemTypes.USER}
-                            open={open}
-                            iconName={'logout'}
-                            userImage={<Avatar imagePath={avatar} size={open ? 'small' : 'xsmall'}/>}
-                            As={() => <Text size="medium">Zhorzhik Grigoryan</Text>}
-                        />
-                    </>
-                </Block>
-            </footer>
-        </SideNavigationItemComp>
-    )
+                  />
+                  <NavigationItem
+                    As={() => <a href="/https://ameriabank.am">Savings</a>}
+                    type={NavigationItemTypes.MAIN}
+                    iconName="savings"
+                    open={open}
+                  />
+                </>
+              )}
+            />
+            <NavigationItem
+              As={() => <a href="/https://ameriabank.am">Savings</a>}
+              type={NavigationItemTypes.MAIN}
+              iconName="savings"
+              open={open}
+              showBadge
+              badgeContent={'888+'}
+            />
+          </>
+        </Block>
+        <Block open={open} label="Finance">
+          <>
+            <NavigationItem
+              As={() => <a href="/https://ameriabank.am">Overdraft</a>}
+              type={NavigationItemTypes.MAIN}
+              iconName="wallet"
+              open={open}
+            />
+            <NavigationItem
+              As={() => <a href="/https://ameriabank.am">Mortgage</a>}
+              type={NavigationItemTypes.MAIN}
+              iconName="home"
+              open={open}
+            />
+            <NavigationItem
+              As={() => <div>Car Leon</div>}
+              type={NavigationItemTypes.MAIN}
+              iconName="savings"
+              open={open}
+              expandable
+              Child={() => (
+                <>
+                  <NavigationItem
+                    As={() => <a href="/https://ameriabank.am">Savings</a>}
+                    type={NavigationItemTypes.MAIN}
+                    iconName="savings"
+                    open={open}
+                  />
+                  <NavigationItem
+                    As={() => <a href="/https://ameriabank.am">Accounts</a>}
+                    type={NavigationItemTypes.MAIN}
+                    iconName="document"
+                    open={open}
+                  />
+                </>
+              )}
+            />
+          </>
+        </Block>
+      </main>
+      <footer>
+        <Block>
+          <>
+            <NavigationItem
+              type={NavigationItemTypes.MAIN}
+              open={open}
+              iconName={'mail'}
+              As={() => <a href="/https://ameriabank.am">Messages</a>}
+            />
+            <NavigationItem
+              type={NavigationItemTypes.MAIN}
+              open={open}
+              iconName={'notification2'}
+              As={() => <a href="https://ameriabank.am">Notifications</a>}
+            />
+            <NavigationItem
+              type={NavigationItemTypes.MAIN}
+              open={open}
+              iconName={'settings'}
+              As={() => <a href="https://ameriabank.am">Settings</a>}
+            />
+            <NavigationItem
+              type={NavigationItemTypes.USER}
+              open={open}
+              iconName={'logout'}
+              userImage={<Avatar imagePath={avatar} size={open ? 'small' : 'xsmall'} />}
+              As={() => <Text size="medium">Zhorzhik Grigoryan</Text>}
+            />
+          </>
+        </Block>
+      </footer>
+    </SideNavigationItemComp>
+  )
 }
 
 export const SideNavigation = Template.bind({})
 
 SideNavigation.args = {
-    showAction: true,
-    actionElm: <Button buttonText={'Action'} size="large" iconProps={{name: 'home'}}/>
+  showAction: true,
+  actionElm: <Button buttonText={'Action'} size="large" iconProps={{ name: 'home' }} />
 }
