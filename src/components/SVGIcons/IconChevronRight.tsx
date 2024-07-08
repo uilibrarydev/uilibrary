79fbf282a11f21e8e567c0d99bb0155d90034b03
Manuk Tagushyan <manuk.tagushyan@gmail.com>
2024-07-08 19:15:03 +0400

    update svg icons

--- src/components/SVGIcons/IconChevronRight.tsx
+++ src/components/SVGIcons/IconChevronRight.tsx
@@ -1,35 +1,24 @@
 import React, { ReactElement } from 'react'
-import { ISVGIconProps } from './types'
+import {ISVGIconProps} from './types';
 import classNames from 'classnames'
 
-export const IconChevronRight = ({
-  size,
-  type,
-  className = '',
-  onClick,
-  refHandler,
-  id,
-  dataId
-}: ISVGIconProps): ReactElement => (
+export const IconChevronRight = ({size, type, className = '', onClick, refHandler, id, dataId }: ISVGIconProps): ReactElement => (
   <svg
-    xmlns="http://www.w3.org/2000/svg"
-    className={classNames('svg-icon', {
-      [`svg-icon__size-${size}`]: size,
-      [`svg-icon__type-${type}`]: type,
-      [className]: className
-    })}
-    viewBox="0 0 24 24"
-    fill="none"
-    onClick={onClick}
-    ref={refHandler}
-    id={id}
-    data-id={dataId ? `${dataId}-svg-icon` : ''}
+      xmlns="http://www.w3.org/2000/svg"
+      className={classNames('svg-icon', {
+          [`svg-icon__size-${size}`]: size,
+          [`svg-icon__type-${type}`]: type,
+          [className]: className
+      })}
+      viewBox="0 0 24 24"
+      fill="none"
+      onClick={onClick}
+      ref={refHandler}
+      id={id}
+      data-id={dataId ? `${dataId}-svg-icon` : ''}
   >
-    <path
-      d="M8.57904 4.21967C8.28615 4.51256 8.28615 4.98744 8.57904 5.28033L15.2987 12L8.57905 18.7197C8.28615 19.0126 8.28615 19.4874 8.57905 19.7803C8.87194 20.0732 9.34681 20.0732 9.63971 19.7803L16.8897 12.5303C17.1826 12.2374 17.1826 11.7626 16.8897 11.4697L9.63971 4.21967C9.34681 3.92678 8.87194 3.92678 8.57904 4.21967Z"
-      fill="#222222"
-    />
+      <path d="M8.57904 4.21967C8.28615 4.51256 8.28615 4.98744 8.57904 5.28033L15.2987 12L8.57905 18.7197C8.28615 19.0126 8.28615 19.4874 8.57905 19.7803C8.87194 20.0732 9.34681 20.0732 9.63971 19.7803L16.8897 12.5303C17.1826 12.2374 17.1826 11.7626 16.8897 11.4697L9.63971 4.21967C9.34681 3.92678 8.87194 3.92678 8.57904 4.21967Z" fill="#222222"/>
   </svg>
-)
+);
 
-export default IconChevronRight
+export default IconChevronRight;
--- src/components/SVGIcons/IconDocumentFilled.tsx
+++ src/components/SVGIcons/IconDocumentFilled.tsx
@@ -1,35 +1,24 @@
 import React, { ReactElement } from 'react'
-import { ISVGIconProps } from './types'
+import {ISVGIconProps} from './types';
 import classNames from 'classnames'
 
-export const IconDocumentFilled = ({
-  size,
-  type,
-  className = '',
-  onClick,
-  refHandler,
-  id,
-  dataId
-}: ISVGIconProps): ReactElement => (
+export const IconDocumentFilled = ({size, type, className = '', onClick, refHandler, id, dataId }: ISVGIconProps): ReactElement => (
   <svg
-    xmlns="http://www.w3.org/2000/svg"
-    className={classNames('svg-icon', {
-      [`svg-icon__size-${size}`]: size,
-      [`svg-icon__type-${type}`]: type,
-      [className]: className
-    })}
-    viewBox="0 0 24 24"
-    fill="none"
-    onClick={onClick}
-    ref={refHandler}
-    id={id}
-    data-id={dataId ? `${dataId}-svg-icon` : ''}
+      xmlns="http://www.w3.org/2000/svg"
+      className={classNames('svg-icon', {
+          [`svg-icon__size-${size}`]: size,
+          [`svg-icon__type-${type}`]: type,
+          [className]: className
+      })}
+      viewBox="0 0 24 24"
+      fill="none"
+      onClick={onClick}
+      ref={refHandler}
+      id={id}
+      data-id={dataId ? `${dataId}-svg-icon` : ''}
   >
-    <path
-      d="M12 2V8C12 9.10457 12.8954 10 14 10H20V20C20 21.1046 19.1046 22 18 22H6C4.89543 22 4 21.1046 4 20V4C4 2.89543 4.89543 2 6 2H12ZM13.5 2.5V8C13.5 8.27614 13.7239 8.5 14 8.5H19.5L13.5 2.5Z"
-      fill="#222222"
-    />
+      <path d="M12 2V8C12 9.10457 12.8954 10 14 10H20V20C20 21.1046 19.1046 22 18 22H6C4.89543 22 4 21.1046 4 20V4C4 2.89543 4.89543 2 6 2H12ZM13.5 2.5V8C13.5 8.27614 13.7239 8.5 14 8.5H19.5L13.5 2.5Z" fill="#222222"/>
   </svg>
-)
+);
 
-export default IconDocumentFilled
+export default IconDocumentFilled;
