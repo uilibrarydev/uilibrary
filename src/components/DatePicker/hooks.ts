24ad28b61fb81667badf9a2cc0b90b5c7a1fa3e6
Manuk Tagushyan <manuk.tagushyan@gmail.com>
2024-08-05 14:33:57 +0400

    Hide warning

--- src/components/DatePicker/hooks.ts
+++ src/components/DatePicker/hooks.ts
@@ -1,4 +1,4 @@
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { useEffect, useState } from 'react'
 import { registerLocale } from 'react-datepicker'
 import { locale } from 'dayjs'
