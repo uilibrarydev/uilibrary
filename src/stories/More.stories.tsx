c8ce9a5da1f59e6c0bf1f7197eee9beddb98bacf
Ramella.Naltakyan <Ramella.Naltakyan@ameriabank.am>
2024-08-29 11:42:08 +0400

    fix: more component ui correction

--- src/stories/More.stories.tsx
+++ src/stories/More.stories.tsx
@@ -9,12 +9,12 @@ const Template = () => {
   return (
     <div onClick={() => console.log('sdjbsd')}>
       <_More
-          buttonText={{
+        buttonText={{
           expand: 'Ավելին',
           collapse: 'Փակել'
         }}
-          collapsedText={'I am informed and agree that:'}
-          expandedText={`I am informed and agree that:
+        collapsedText={'I am informed and agree that:'}
+        expandedText={`I am informed and agree that:
  - The vacation application is considered submitted if it has been approved by the manager(s) and substitute(s). If the approvals haven’t been obtained, the application cannot be considered by the HRM and is not subject to processing.
 The payment for the annual vacation is made:
 3 days before the start of the vacation, if the application approvals are available at least 6 working days before the vacation begins,
