b8f3217f6fc7069c558a4d159c19724554250bae
Luiza Shirmazanyan <luizashirmazanyan@Luizas-MacBook-Pro.local>
2024-10-29 15:48:45 +0400

    fix: close profile dropdown on item click

--- src/components/Select/ProfileDropdown/index.tsx
+++ src/components/Select/ProfileDropdown/index.tsx
@@ -48,7 +48,6 @@ export const ProfileDropdown = (props: TProfileDropdownProps): JSX.Element => {
                       </>
                     </Link>
                   ) : (
-                    // item.children
                     <div onClick={closeDropdown}>{item.children}</div>
                   )}
                 </li>
