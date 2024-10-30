dd632d05072a8b1d0a36bb092604fafcee58e882
Ramella.Naltakyan <Ramella.Naltakyan@ameriabank.am>
2024-10-30 19:23:30 +0400

    fix: fixed itemSelect icon props issue

--- src/components/ItemSelect/ItemSelect.tsx
+++ src/components/ItemSelect/ItemSelect.tsx
@@ -1,13 +1,12 @@
 import React, { ReactElement } from 'react'
 import { Text } from '../Text'
 import { TItemSelectProps } from './types'
-import classNames from 'classnames'
 import { Image } from '../Image'
 import { Radio } from '../Radio'
+import classNames from 'classnames'
 
 export const ItemSelect = (props: TItemSelectProps): ReactElement => {
   const {
-    rightIconProps,
     leftIconProps,
     label,
     image,
@@ -17,29 +16,29 @@ export const ItemSelect = (props: TItemSelectProps): ReactElement => {
     name,
     value,
     itemValue,
-    setFieldValue
+    setFieldValue,
+    icon
   } = props
   const selected = itemValue === value
   const itemSelectStyle = classNames(
     `item-select ${disabled ? 'item-select--disabled' : selected ? 'item-select--selected' : ''}`,
     className
   )
-  const iconProps = selected ? rightIconProps : undefined
-  const handleCardSelectValue = (selected: number | string) => {
+  const handleItemSelectValue = (selected: number | string) => {
     if (name && setFieldValue) {
       setFieldValue(name, selected, { shouldValidate: true })
     }
     handleChange?.(selected)
   }
 
-  const handleCardSelect = () => {
+  const handleItemSelect = () => {
     if (!disabled && itemValue) {
-      handleCardSelectValue(itemValue)
+      handleItemSelectValue(itemValue)
     }
   }
 
   return (
-    <div className={itemSelectStyle} onClick={handleCardSelect}>
+    <div className={itemSelectStyle} onClick={handleItemSelect}>
       {image || leftIconProps ? (
         <div className={'item-select__left mr-12'}>
           {image ? (
@@ -58,14 +57,10 @@ export const ItemSelect = (props: TItemSelectProps): ReactElement => {
           <>{label}</>
         </Text>
       </div>
-      <Radio
-        value={itemValue}
-        name={name}
-        isSelected={selected}
-        disabled={disabled}
-        className={'ml-16'}
-        iconProps={iconProps}
-      />
+      { icon && icon.Component ? (
+          selected ? <icon.Component {...icon} /> : null
+        ) : <Radio name={name} isSelected={selected} disabled={disabled} className={'item-select__radio ml-16'} />
+      }
     </div>
   )
 }
--- src/components/ItemSelect/types.ts
+++ src/components/ItemSelect/types.ts
@@ -2,22 +2,22 @@ import { ReactElement } from 'react'
 export interface TItemSelectProps extends IFormCompProps {
   id?: number | string
   image?: string
-  rightIconProps?: ReactElement
   label: string
   leftIconProps?: ReactElement
   className?: string
   disabled?: boolean
-  handleCardSelectValue?: (isChecked: boolean) => void
+  handleItemSelectValue?: (isChecked: boolean) => void
   value?: number | string
   handleChange?: (selected: number | string) => void
   isSelected?: boolean
   itemValue?: number | string
+  icon?: TSelectIconProps
 }
 export interface TItemSelectGroupProps extends IFormCompProps {
   isHorizontal?: boolean
   className?: string
   items: TItemSelectProps[]
+  iconGroup?: TSelectIconProps
   handleChange?: (selected: number | string) => void
   value: number | string
-  rightIconProps?: ReactElement
 }
