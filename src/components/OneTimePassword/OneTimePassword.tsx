94c9e0bceb2b2dc83268a54de687cd5e0a2eaaf8
Garik  Grigoryan <garikgrigoryan@MacBook-Pro-Garik.local>
2024-11-13 16:23:21 +0400

    feat: otp component creation

---
+++ src/components/OneTimePassword/OneTimePassword.tsx
@@ -0,0 +1,99 @@
+import React, { useState, useRef, KeyboardEvent } from 'react'
+import classNames from 'classnames'
+import { Input } from '../Input'
+import { OtpCustomProps } from './types'
+import { ErrorMessage, Label } from '../../helperComponents'
+import { Text } from '../Text'
+import IconCheckmarkCircleFilled from '../SVGIcons/IconCheckmarkCircleFilled'
+
+export const OneTimePassword = React.forwardRef<HTMLInputElement, OtpCustomProps>(
+  ({
+    className,
+    size = 'large',
+    error,
+    hasError,
+    label,
+    name,
+    disabled,
+    required,
+    placeholder,
+    type,
+    setFieldValue,
+    handleChange,
+    dataId = '',
+    isValid,
+    length,
+    successMessage,
+    ...rest
+  }): JSX.Element => {
+    const isErrorVisible = hasError !== undefined ? hasError : !!error
+    const [otp, setOtp] = useState<string[]>(Array(length).fill(''))
+    const inputRefs = useRef<(HTMLInputElement | null)[]>([])
+
+    const onChange = (e: TChangeEventType, value: string, index: number) => {
+      if ((!!Number(value) && value.length <= 1) || value === '') {
+        const newOtp = [...otp]
+        newOtp[index] = value?.slice(-1)
+        setOtp(newOtp)
+        const currentValue = newOtp.join('')
+        if (handleChange) {
+          handleChange(e, currentValue)
+        }
+        if (setFieldValue && name) {
+          setFieldValue(name, currentValue)
+        }
+        if (value && index < length - 1) {
+          inputRefs.current[index + 1]?.focus()
+        }
+      } else return
+    }
+
+    const handleKeyDown = (event: KeyboardEvent<HTMLInputElement>, index: number) => {
+      if (event.key === 'Backspace' && !otp[index] && index > 0) {
+        inputRefs.current[index - 1]?.focus()
+      }
+    }
+
+    return (
+      <div className={classNames('otp', className, { 'otp--invalid': hasError })}>
+        <Label text={label} required={required} disabled={disabled} dataId={dataId} />
+        <div className="otp--input-wrapper">
+          {otp.map((digits, index) => (
+            <Input
+              key={index}
+              size={size}
+              ref={(el) => (inputRefs.current[index] = el)}
+              max={1}
+              type={type}
+              isValid={isValid}
+              readonly={disabled}
+              disabled={disabled}
+              placeholder={placeholder}
+              currentValue={digits}
+              handleChange={(e, value) => onChange(e, value, index)}
+              onKeyDown={(e) => handleKeyDown(e, index)}
+              {...rest}
+            />
+          ))}
+        </div>
+        {error || successMessage ? (
+          <div className="input__message mt-8">
+            {isErrorVisible && error ? (
+              <ErrorMessage message={error} icon="infoFilled" dataId={dataId} />
+            ) : null}
+            {successMessage ? (
+              <Text size="small" type="success" className="flexbox align-items--center">
+                <>
+                  <IconCheckmarkCircleFilled type="success" size="xsmall" />
+                  <span className="ml-4">{successMessage}</span>
+                </>
+              </Text>
+            ) : null}
+          </div>
+        ) : null}
+      </div>
+    )
+  }
+)
+
+OneTimePassword.displayName = 'OneTimePassword'
---
+++ src/stories/OneTimePassword.stories.tsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import { OneTimePassword as _OneTimePassword } from '../index'
+import { StoryFn } from '@storybook/react'
+import { OtpCustomProps } from '../components/OneTimePassword/types'
+
+export default {
+  title: 'OneTimePassword',
+  component: _OneTimePassword,
+  argTypes: {
+    type: {
+      options: ['text', 'password'],
+      control: { type: 'radio' }
+    },
+    size: {
+      options: ['large', 'medium', 'small'],
+      control: { type: 'radio' }
+    },
+    length: {
+      options: [4, 6, 8],
+      control: { type: 'radio' }
+    },
+    hasError: {
+      options: [true, false],
+      control: { type: 'radio' }
+    }
+  }
+}
+
+const Template: StoryFn<OtpCustomProps> = (args) => {
+  return <_OneTimePassword {...args} />
+}
+
+export const OneTimePassword = Template.bind({})
+
+// @ts-ignore
+OneTimePassword.args = {
+  type: 'text',
+  size: 'large',
+  length: 4,
+  label: 'One Time Password',
+  hasError: true,
+  error: 'error text',
+  successMessage: 'Success text'
+}
