263ad718c4aaf3c5c5976f8f3151deda0073528b
Ramella.Naltakyan <Ramella.Naltakyan@ameriabank.am>
2024-11-15 14:49:41 +0400

    feat: added framedIcon component, updte grey-30 and grey-40 tokens value

---
+++ src/components/FramedIcon/consts.ts
@@ -0,0 +1,12 @@
+import type { TSVGIconSize } from '../SVGIcons/types'
+
+export const FRAME_SIZE_MAPPING: { [key: string]: TSVGIconSize } = {
+  large: 'large',
+  medium: 'medium',
+  small: 'small',
+}
+export const ICON_SIZE_MAPPING: { [key: string]: TSVGIconSize } = {
+  large: 'xxlarge',
+  medium: 'xlarge',
+  small: 'medium',
+}
