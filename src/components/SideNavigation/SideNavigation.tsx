3a2975b88c32c25b91f9d5f5f01d68abd605df56
Garik  Grigoryan <garikgrigoryan@MacBook-Pro-Garik.local>
2025-01-09 10:52:57 +0400

    fix: show hide toggle button

--- src/components/SideNavigation/SideNavigation.tsx
+++ src/components/SideNavigation/SideNavigation.tsx
@@ -5,20 +5,23 @@ import IconChevronRight from '../SVGIcons/IconChevronRight'
 import { Button } from '../Button'
 
 export const SideNavigation = (props: TSideNavigationPropTypes): JSX.Element => {
-  const { children, isOpen, setOpen } = props
+  const { children, isOpen = true, setOpen, showToggleIcon = true } = props
 
   const handleToggleOpen = () => {
-    setOpen(!isOpen)
+    setOpen?.(!isOpen)
   }
 
   return (
     <div className={classNames('side-navigation', isOpen ? 'side-navigation__opened' : null)}>
-      <Button
-        iconProps={{ Component: IconChevronRight }}
-        size={'small'}
-        className={'side-navigation__btn'}
-        onClick={handleToggleOpen}
-      />
+      {showToggleIcon && (
+        <Button
+          iconProps={{ Component: IconChevronRight }}
+          size={'small'}
+          className={'side-navigation__btn'}
+          onClick={handleToggleOpen}
+        />
+      )}
+
       {children}
     </div>
   )
