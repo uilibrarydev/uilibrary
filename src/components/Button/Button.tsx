1d386c8278e59651a510a50cd5dbc6691995b379
Karo <karo.ghulyan@ameriabank.am>
2025-02-13 09:47:45 +0400

    fix: remove is loading

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -29,7 +29,7 @@ export const Button = (props: TButtonPropTypes): ReactElement => {
     <button
       ref={refHandler}
       data-id={dataId}
-      disabled={disabled || isLoading}
+      disabled={disabled}
       type={buttonActionType}
       className={classnames(
         'btn',
