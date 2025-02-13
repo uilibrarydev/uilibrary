6bd55f1ef821547ca8e262b92651a43722ca6e60
Karo <karo.ghulyan@ameriabank.am>
2025-02-13 18:01:03 +0400

    feat: add link properties for alert

--- src/components/Alert/types.ts
+++ src/components/Alert/types.ts
@@ -1,4 +1,5 @@
 import type { TButtonPropTypes } from '../Button/types'
+import type { LinkPropTypes } from '../Link/types'
 
 export type TAlertTypes = 'grey' | 'success' | 'error' | 'warning' | 'information' | 'discovery'
 export type TAlertPosition = 'inline' | 'sticky'
@@ -7,12 +8,13 @@ export interface TAlertProps extends IFormCompProps {
   type?: TAlertTypes
   position?: TAlertPosition
   text: string | JSX.Element
-  subtext: string | JSX.Element
+  subtext?: string | JSX.Element
   closeIcon?: boolean
-  onClose: () => void
+  onClose?: () => void
   buttonProps?: {
     confirm: TButtonPropTypes
     cancel?: TButtonPropTypes
   }
   className?: string
+  linkAddons?: LinkPropTypes
 }
